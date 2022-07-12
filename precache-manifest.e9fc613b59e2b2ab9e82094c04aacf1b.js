5ce85c075d39efd0a31f9aad33fe2e3328549cd9
Ahzam Syed <ahzam183@gmail.com>
2022-07-12 22:35:44 +0530

    Updates

---
+++ precache-manifest.e9fc613b59e2b2ab9e82094c04aacf1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36e2d56e519801f01402",
+    "url": "/textutils/static/css/main.6629e245.chunk.css"
+  },
+  {
+    "revision": "36e2d56e519801f01402",
+    "url": "/textutils/static/js/main.85e25cc8.chunk.js"
+  },
+  {
+    "revision": "8a1d1f539275a0dd44f5",
+    "url": "/textutils/static/js/runtime~main.ec408e75.js"
+  },
+  {
+    "revision": "ee7f37ea120d5f770ab3",
+    "url": "/textutils/static/js/2.18feb4ae.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/textutils/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "a236565367a4684e5755bcbbcdd6b3a8",
+    "url": "/textutils/index.html"
+  }
+];
